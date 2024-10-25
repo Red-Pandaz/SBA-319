7bc7472d811ee33951e55904fba076fd452219e7
Red Panda <Deep_Sea_Penguin@protonmail.com>
2024-10-25 19:50:04 -0400

    basic server config

---
+++ server.mjs
@@ -0,0 +1,20 @@
+//imports
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+// import connectDB from './db/conn.mjs'
+
+//setups
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 3001;
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+//routes
+// connectDB()
+//listener
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port: ${PORT}`);
+})
